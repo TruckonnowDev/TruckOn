857255774cbc48a03de03035923ad9dd0901623f
Дмитрий Медведев <greatsmith184@gmail.com>
2022-05-10 16:15:38 +0300

    Added constants to TruckAndTrailerService.

--- WebDispacher/Constants/DocAndFileConstants.cs
+++ WebDispacher/Constants/DocAndFileConstants.cs
@@ -3,6 +3,14 @@
     public static class DocAndFileConstants
     {
         public const string ContentTypePdf = "application/pdf";
+        public const string TruckRegistration = "Truck registration";
+        public const string TrailerRegistration = "Trailer registration";
+        public const string TruckAgreement = "Truck lease agreement";
+        public const string TruckInspection = "Truck annual inspection";
+        public const string TrailerInspection = "Trailer annual inspection";
+        public const string LeaseAgreement = "Lease agreement";
+        public const string BobTailPhysicalDamage = "Bob tail physical damage";
+        public const string NyHit = "NY HUT";
         public const string True = "true";
         public const string False = "false";
         public const string NoneLevel = "None";

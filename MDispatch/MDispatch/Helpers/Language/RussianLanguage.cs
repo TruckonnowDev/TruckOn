7bf9e4796b21691fe66eaeb5a95be3f447d55589
Alen Beglaryan <53264155+Wolige@users.noreply.github.com>
2022-05-15 03:11:34 +0400

    Работа с языками

--- MDispatch/MDispatch/Helpers/Language/RussianLanguage.cs
+++ MDispatch/MDispatch/Helpers/Language/RussianLanguage.cs
@@ -85,7 +85,7 @@ namespace MDispatch.Helpers.Language
 
         public string ContinuingInspectionDelivery => "Продолжение осмотра при доставке";
         public string ContinuingInspectionPickedUp => "Постоянное осмотр Погрузки";
-        public string StartInspectionDelivery => "Начать инспекци доставку";
+        public string StartInspectionDelivery => "Начать Инспекцию на Доставке";
         public string StartInspectionPickedUp => "Начать осмотр Полученния";
 
         public string HintPhotoItemsPage => "Пожалуйста, сфотографируйте предметы, которые перевозятся на машине.";
@@ -108,10 +108,10 @@ namespace MDispatch.Helpers.Language
 
         public string AskBlockSafeLoctionTitle => "Безопасное место доставки";
 
-        public string AskBlockFarFromTrailerTitle => "Как далеко от трейлера?";
-        public string PlaceholderFarFromTrailerAnswer => "Пожалуйста, укажите, как далеко вы находитесь от трейлера";
+        public string AskBlockFarFromTrailerTitle => "Как далеко вы от трейлера?";
+        public string PlaceholderFarFromTrailerAnswer => "Укажите в ft.";
 
-        public string AskBlockGaveKeysTitle => "Пожалуйста, введите, как далеко вы от трейлера";
+        public string AskBlockGaveKeysTitle => "Пожалуйста, укажите кто отдал вам машину и ключи";
         public string PlaceholderGaveKeysAnswer => "Введите полное имя";
 
         public string AskBlockEnoughSpaceTitle => "Достаточно места для фотографий?";
@@ -144,7 +144,7 @@ namespace MDispatch.Helpers.Language
         public string NextBtnText => "Следующий";
         public string AddPhotoText => "Добавить фото";
 
-        public string AskBlockJumpedVehicleTitle => "Вы исполузували кабель чтобы запустить машину?";
+        public string AskBlockJumpedVehicleTitle => "Вы использовали кабель чтобы запустить авто?";
 
         public string AskBlockMusteMileageTitle => "Точный пробег после загрузки (необходимо ввести мили)";
         public string PlaceholderMusteMileage => "Пробег";
@@ -247,7 +247,7 @@ namespace MDispatch.Helpers.Language
 
         public string AskBlockRateCustomerTitle => "Оцените клиента";
 
-        public string ComleteInspactinBtnText => "Завершит осмотр";
+        public string ComleteInspactinBtnText => "Завершить Осмотр";
 
         public string TitleBlockInspaction => "Осмотр водителя";
         public string TimeInspactionText => "Время до осмотра: ";
@@ -319,7 +319,7 @@ namespace MDispatch.Helpers.Language
         public string AskBlockRateDriverTitle => "Оценить водителя";
 
         public string VehicleInspectionPikedUp => "Начать Осмотр на Получении";
-        public string VehicleInspectionDelivery => "Техосмотр автомобил: Delivery";
+        public string VehicleInspectionDelivery => "Начать Осмотр на Доставке";
         public string ThereAreNoVehiclesInThisOrder => "В этом заказе нет транспортных средств";
 
         public string TitelSelectPickPhoto => "Выберете вариант";
